53e8fdca11cf4320a7902c8d5c7ef5cabab885db
CasstielP <cpss5433@gmail.com>
2024-11-01 13:06:41 -0700

    add validatiing login request body logic

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,14 +5,27 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const {check} = require('express-validator')
+const { handleValidationErrors } = require('../../utils/validation');
 
-
+//method: to validate login request body
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
 
 
 
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
