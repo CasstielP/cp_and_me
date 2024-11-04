af37258d2748e3297905e3b45da7e81dc3d68a38
CasstielP <cpss5433@gmail.com>
2024-11-03 19:54:32 -0800

    add restore session thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,18 @@ const removeUser = () => {
   };
 };
 
+//restore user
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+
+
+
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
