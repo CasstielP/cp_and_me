46dc8f8d9fa480de84e681e02646ca03b67e8d35
CasstielP <cpss5433@gmail.com>
2024-11-04 11:08:10 -0800

    add navigation functionality and profile button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import * as sessionActions from './store/session'
-import SignupFormPage from './components/SignupFormPage';
-
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
+import SignupFormPage from "./components/SignupFormPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,18 +13,22 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-
-
-
-  return isLoaded && (
-    <Switch>
-      <Route path='/login'>
-        <LoginFormPage />
-      </Route>
-      <Route>
-        <SignupFormPage path='/signup'/>
-      </Route>
-    </Switch>
+  return (
+    isLoaded && (
+      <>
+        <Navigation isLoaded={isLoaded} />
+        {isLoaded && (
+          <Switch>
+            <Route path="/login">
+              <LoginFormPage />
+            </Route>
+            <Route>
+              <SignupFormPage path="/signup" />
+            </Route>
+          </Switch>
+        )}
+      </>
+    )
   );
 }
 
